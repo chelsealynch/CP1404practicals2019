c758b99631011b21ce34e4baf66996bcdfa268db
jc493233 <chelsea171100@gmail.com>
2019-04-11 13:40:08 +1000

    Fix formatting issues

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,8 +1,8 @@
 COLOURS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
-                "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
-                "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
-                "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
-                "aquamarine4": "#458b74", "azure1": "#f0ffff"}
+           "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
+           "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
+           "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
+           "aquamarine4": "#458b74", "azure1": "#f0ffff"}
 
 colour_name = input("Enter the name of a colour: ")
 while colour_name != "":
