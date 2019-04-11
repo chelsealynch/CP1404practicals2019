9b6f3d7f7d076861a8320f810ac76b83a6328bf1
jc493233 <chelsea171100@gmail.com>
2019-04-11 13:45:27 +1000

    Initial commit

---
+++ prac_05/extension_work.py
@@ -0,0 +1,5 @@
+# names = ["Jack", "Jill", "Harry"]
+# dates_of_birth = [(12, 4, 1999), (1, 1, 2000), (27, 3, 1982)]
+
+NAMES_AND_DOB = {"Jack": "12/4/1999", "Jill": "1/1/2000", "Harry": "27/3/1982"}
+
