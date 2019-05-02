8c120bc3ce65c34a65aa1d2a2f5c641418e41a15
jc493233 <chelsea171100@gmail.com>
2019-05-02 14:48:22 +1000

    Change expectations

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -12,9 +12,9 @@ def run_tests():
     guitar = Guitar(name, year, cost)
     other = Guitar("Another Guitar", 2012, 1512.9)
 
-    print("{} get_age() - Expected {}. Got {}".format(guitar.name, 95,
+    print("{} get_age() - Expected {}. Got {}".format(guitar.name, 97,
                                                       guitar.get_age()))
-    print("{} get_age() - Expected {}. Got {}".format(other.name, 5,
+    print("{} get_age() - Expected {}. Got {}".format(other.name, 7,
                                                       other.get_age()))
     print()
     print("{} is_vintage() - Expected {}. Got {}".format(guitar.name,
