5c99bab0a1f931ee688261d4666b177db3ae042f
jc493233 <chelsea171100@gmail.com>
2019-05-02 13:37:41 +1000

    Initial commit

---
+++ prac_07/score_calculator.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class ScoreCalculator(App):
+    def build(self):
+        self.title = "Score Calculator"
+        self.root = Builder.load_file('score_calculator.kv')
+        return self.root
+
+    def calculate_score(self):
+        if self.root.ids.input_score.text < 50:
+            self.root.ids.output_label.text = "Fail"
+        elif self.root.ids.input_score.text >= 50:
+            self.root.ids.output_label.text = "Pass"
+
+    def clear_text(self):
+        self.root.ids.score_label.text = "Enter your score"
+        self.root.ids.input_score.text = ""
+
+
+ScoreCalculator().run()
