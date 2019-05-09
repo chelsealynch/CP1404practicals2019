e438c92a789e8dea6aece6fdee7112952eb7ceb3
jc493233 <chelsea171100@gmail.com>
2019-05-09 14:55:20 +1000

    Add methods to __init__ function

--- prac_08/silver_serivce_taxi.py
+++ prac_08/silver_serivce_taxi.py
@@ -4,3 +4,6 @@ from prac_08.taxi import Taxi
 class SilverServiceTaxi(Taxi):
 
     def __init__(self, name, fuel, fanciness):
+        self.name = name
+        self.fuel = fuel
+        self.fanciness = fanciness
