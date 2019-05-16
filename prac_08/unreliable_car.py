2f1c59071435a9c6b05b02c71a4568aa8ce59260
jc493233 <chelsea171100@gmail.com>
2019-05-16 14:56:37 +1000

    Call super to inherit from parent class

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -5,13 +5,12 @@ from random import randint
 class UnreliableCar(Car):
 
     def __init__(self, name, fuel, reliability):
-        self.name = name
-        self.fuel = fuel
+        super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
         car_reliability = randint(0, 100)
         if car_reliability >= self.reliability:
             distance = 0
-        distance_driven = self.drive(distance)
+        distance_driven = super().drive(distance)
         return distance_driven
