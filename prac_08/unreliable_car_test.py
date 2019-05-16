48674eeac9a3ea06e70a22ee1b436b592a53530b
jc493233 <chelsea171100@gmail.com>
2019-05-16 14:55:46 +1000

    Fix tests and add functionality

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -3,6 +3,11 @@ from prac_08.unreliable_car import UnreliableCar
 car1 = UnreliableCar("Reliable", 100, 80)
 car2 = UnreliableCar("Unreliable", 100, 10)
 
-for i in range(1, 50):
-    print("Reliable car drove {}km".format(i))
-    print("Unreliable car drove {}km".format(i))
+for i in range(1, 100):
+    distance = car1.drive(1)
+    print("Reliable car drove {}km".format(distance), end=" ")
+    distance = car2.drive(1)
+    print("Unreliable car drove {}km".format(distance))
+
+print(car1)
+print(car2)
